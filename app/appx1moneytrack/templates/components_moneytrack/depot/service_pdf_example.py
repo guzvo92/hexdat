13776a659261339ed4dfd0a16434f5a270147866
gman <guzvo92@gmail.com>
2020-11-18 17:26:30 -0600

    [app1moneytrack/v12] edicion de reporte-pdf version 1

--- app/appx1moneytrack/templates/components_moneytrack/depot/service_pdf_example.py
+++ app/appx1moneytrack/templates/components_moneytrack/depot/service_pdf_example.py
@@ -5,19 +5,14 @@ from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from django.http import HttpResponse
-from .services import *
-from .models import *
 
 
 def report_fn():
 
-    registros = Registrogasto.objects.all()
-    datostotales = totalgastos()
-
     #creando los httpresponse headers con pdf
     #response = HttpResponse("hello world")
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename=Reporte_Gastos.pdf'
+    response['Content-Disposition'] = 'attachment; filename=ReporteGli.pdf'
 
     #Creater el objeto pdf usando el "objeto BytesIO" como archivo
     buffer = BytesIO()
@@ -26,31 +21,10 @@ def report_fn():
     #creando los headers
     c.setLineWidth(.3)
     c.setFont('Helvetica',22)
-    c.drawString(30,750,'Reporte de gastos')
+    c.drawString(30,750,'GMAN')
     c.setFont('Helvetica-Bold',12)
     c.drawString(480,750, "01/11/2020")
     c.line(460,747,560,747) #(x,height,y,height)
-
-    x=50
-    y=700
-    count = 1
-
-    for reg in registros:
-        fecha_cut = (str(reg.created_at)[0:16])
-
-        c.drawString(x,y, str(count) )
-        x += 20
-        c.drawString(x,y, str(reg.quantity) )
-        x += 40
-        c.drawString(x,y, str(reg.category) )
-        x += 100
-        c.drawString(x,y, str(reg.concept))
-        x += 200
-        c.drawString(x,y, str(fecha_cut))
-        y -= 50
-        x = 50
-        count += 1
-
     c.save()
 
     pdf =buffer.getvalue()
