3c8fb1c779df396adc87559ae13ddaeb6710a539
gman <guzvo92@gmail.com>
2020-11-18 22:01:29 -0600

    [app1moneytrack/v13] edicion de reporte-pdf version 2

--- app/appx1moneytrack/service_pdf.py
+++ app/appx1moneytrack/service_pdf.py
@@ -7,6 +7,7 @@ from reportlab.lib.pagesizes import A4
 from django.http import HttpResponse
 from .services import *
 from .models import *
+from datetime import datetime, timedelta
 
 
 def report_fn():
@@ -21,14 +22,18 @@ def report_fn():
 
     #Creater el objeto pdf usando el "objeto BytesIO" como archivo
     buffer = BytesIO()
+    image = 'static/appx1moneytrack/logohexdat_white.png'
     c = canvas.Canvas(buffer, pagesize=A4)
 
+    tiemponow = (datetime.today()).strftime("%Y-%m-%d %H:%M")
+
     #creando los headers
+    c.drawInlineImage(image, 40, 745, 100, 30)
     c.setLineWidth(.3)
     c.setFont('Helvetica',22)
-    c.drawString(30,750,'Reporte de gastos')
+    c.drawString(200,750,'Reporte de gastos')
     c.setFont('Helvetica-Bold',12)
-    c.drawString(480,750, "01/11/2020")
+    c.drawString(463,750, str(tiemponow)  )
     c.line(460,747,560,747) #(x,height,y,height)
 
     x=50
@@ -47,7 +52,7 @@ def report_fn():
         c.drawString(x,y, str(reg.concept))
         x += 200
         c.drawString(x,y, str(fecha_cut))
-        y -= 50
+        y -= 30
         x = 50
         count += 1
 
