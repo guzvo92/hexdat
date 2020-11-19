cd119af87030abb9f70ecc66285b064199cac4f8
gman <guzvo92@gmail.com>
2020-11-19 17:13:40 -0600

    [app1moneytrack/v15] reportes con indicadores

--- app/appx1moneytrack/service_pdf.py
+++ app/appx1moneytrack/service_pdf.py
@@ -13,10 +13,13 @@ from datetime import datetime, timedelta
 def report_fn():
 
     registros = Registrogasto.objects.all()
-    datostotales = totalgastos()
-    
+    datostotalesx = totalgastos()
+    print()
+    #print(datostotalesx.get(total_comida))
+    #print(datostotalesx.total_despensa)
+
     tiemponow = (datetime.today()).strftime("%Y-%m-%d %H:%M")
-    namepdf = "RG_" + str(tiemponow)
+    namepdf = "RG." + str(tiemponow)
 
     #creando los httpresponse headers con pdf
     #response = HttpResponse("hello world")
@@ -29,28 +32,46 @@ def report_fn():
     image = 'static/appx1moneytrack/logohexdat_white.png'
     c = canvas.Canvas(buffer, pagesize=A4)
 
-    #creando los headers  
+    #creando los headers
     c.drawInlineImage(image, 40, 745, 100, 30)
     c.setLineWidth(.3)
     c.setFont('Helvetica',22)
     c.drawString(200,750,"Reporte de Gastos")
     c.setFont('Helvetica-Bold',12)
-    c.drawString(463,750, str(tiemponow)  )
-    c.line(460,747,560,747) #(x,height,y,height)
+    c.drawString(443,750, str(tiemponow)  )
+    c.line(420,747,560,747) #(x,height,y,height)
 
+    #haciendo el bloque de indicadores
+
+    CO1x = [ 50 , 150,  250, 360, 450]
+
+    c.drawString(CO1x[0],700,"Comida")
+    c.drawString(CO1x[1],700,"Despensa")
+    c.drawString(CO1x[2],700,"Servicios")
+    c.drawString(CO1x[3],700,"Extras")
+    c.drawString(CO1x[4],700,"Total Gastos")
+
+    CO2x = [ 64 , 165,  268, 365, 475]
+
+    c.drawString(CO2x[0],680,str(datostotalesx.get("total_comida")))
+    c.drawString(CO2x[1],680,str(datostotalesx.get("total_despensa")))
+    c.drawString(CO2x[2],680,str(datostotalesx.get("total_servicios")))
+    c.drawString(CO2x[3],680,str(datostotalesx.get("total_extras")))
+    c.drawString(CO2x[4],680,str(datostotalesx.get("total_neto")))
+
+    #desplegando los registros
     x=50
-    y=700
+    y=600
     count = 1
-
     for reg in registros:
         fecha_cut = (str(reg.created_at)[0:16])
 
         c.drawString(x,y, str(count) )
-        x += 20
+        x += 40
         c.drawString(x,y, str(reg.quantity) )
         x += 40
         c.drawString(x,y, str(reg.category) )
-        x += 100
+        x += 120
         c.drawString(x,y, str(reg.concept))
         x += 200
         c.drawString(x,y, str(fecha_cut))
