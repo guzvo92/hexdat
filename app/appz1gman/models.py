b8ed7bdfdc4c56c7ac8c2a6edbe25e3f21a0f7e3
gman <guzvo92@gmail.com>
2020-12-16 23:42:21 -0600

    [appz1gman/v15] masterchange agregada logica de navegacion inferior, conteo prev/next, modelo con contador y reserteo de ID en campo NUM

--- app/appz1gman/models.py
+++ app/appz1gman/models.py
@@ -18,3 +18,18 @@ class Registroproyecto(models.Model):
 
     #created_at= models.DateTimeField(auto_now_add=True)
     #updated_at = models.DateTimeField(auto_now=True)
+
+
+class FinalRegistroproyecto(models.Model):
+    num = models.IntegerField(default=0)
+    prev = models.IntegerField(default=0)
+    next = models.IntegerField(default=0)
+    title = models.CharField(max_length=60)
+    description = models.CharField(max_length=150)
+    details = models.CharField(default='null', max_length=500)
+    category = models.CharField(default='null', max_length=100)
+    image1 = models.ImageField(default='null',upload_to ="projectsgman")
+    image2 = models.ImageField(default='null',upload_to ="projectsgman")
+    image3 = models.ImageField(default='null',upload_to ="projectsgman")
+    image4 = models.ImageField(default='null',upload_to ="projectsgman")
+    image5 = models.ImageField(default='null',upload_to ="projectsgman")
--- app/appz1gman/views.py
+++ app/appz1gman/views.py
@@ -51,19 +51,62 @@ def rowevaluate():
 	print("[R4]" + str(R4) + " [MAX] = "+ str(maxr4))
 
 	maxarray = [ maxr1, maxr2, maxr3, maxr4]
-
 	return maxarray
 
+#eval = rowevaluate() #paso array con conteos de renglon YA NO SE USO
 
 def index_gman(request):
-	projectlist = Registroproyecto.objects.all
-	eval = rowevaluate() #paso array con conteos
-	conteo = 0
+	projectlist1 = Registroproyecto.objects.all()
+	db1 = Registroproyecto.objects.count()
+	db2 = FinalRegistroproyecto.objects.count()
+	conteo = 1
 
+	if db1 != db2:
+		print("[GMAN] -> Diferencia de DBs detectada")
+		FinalRegistroproyecto.objects.all().delete()
+
+		for x in projectlist1:
+			if conteo > 1:
+				prevx = conteo - 1
+			else: prevx = 1
+
+			if conteo == db1:
+				nextx = conteo
+			else: nextx = conteo + 1
+			XFinalRegistroproyecto = FinalRegistroproyecto(
+				num = conteo,
+				next = nextx,
+				prev = prevx,
+				title = x.title,
+				description = x.description,
+				details = x.details,
+				category = x.category,
+				image1 = x.image1,
+				image2 = x.image2,
+				image3 = x.image3,
+				image4 = x.image4,
+				image5 = x.image5,
+				)
+			XFinalRegistroproyecto.save()
+			print(str(XFinalRegistroproyecto.num) + " / " + str(XFinalRegistroproyecto))
+			conteo += 1
+	else: print("[GMAN] -> No hay diferencias de DBs")
+
+	projectlist2 = FinalRegistroproyecto.objects.all()
 	return render(
 		request,'index_gman.html',
-		{'projectlistx':projectlist,'evalx':eval,'conteox':conteo}
+		{'projectlistx':projectlist2,'conteox':conteo}
 	)
+
+
+def getproject(request,idx):
+	#recordar que num no es el pk la db que uso es la recorrida
+    project = FinalRegistroproyecto.objects.get(num=idx)
+    return render(
+		request,'get_Project_gman.html',
+		{'projectx':project,}
+	)
+
 
 def admin_gman(request):
     projectlist = Registroproyecto.objects.all
@@ -104,7 +147,7 @@ def edit_project(request,idx):
 	)
 
 def save_edit_project(request,idx):
-	registro = Registroproyecto.objects.get(pk=idx)
+	registro = FinalRegistroproyecto.objects.get(num=idx)
 	registro.title = request.POST['newreg_title']
 	registro.description = request.POST['newreg_description']
 	registro.details = request.POST['newreg_details']
@@ -112,11 +155,3 @@ def save_edit_project(request,idx):
 	registro.save()
 	print("Proyecto editado: " + registro.title + "/" +  registro.category )
 	return redirect ('admin_gman')
-
-
-def getproject(request,idx):
-    project = Registroproyecto.objects.get(pk=idx)
-    return render(
-		request,'get_Project_gman.html',
-		{'projectx':project,}
-	)
